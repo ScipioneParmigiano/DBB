345fca31fcac5081d6ebd2f5b97ad1f7cd78e0c9
ScipioneParmigiano <pcs.parmigiano@gmail.com>
2024-07-06 11:31:59 +0200

    \frac{P(s)^2}{Q(s)} constraint

--- c.cpp
+++ c.cpp
@@ -12,22 +12,8 @@ using namespace monty;
 int main() {
     int n = 3;  // Example dimension
     int m = 2;   // Number of assets
-    double alpha = 1;  // Example value of alpha
-    std::vector<int> omega_l = {0,1, 2};  // Example mask omega_l: select the states of interest
-
-    // std::vector<std::vector<double>> payoff_matrix(m, std::vector<double>(n, 1.0)); // Example matrix, initialized to ones
-    // std::vector<std::vector<double>> payoff_matrix(m, std::vector<double>(n));
-
-    // std::random_device rd;
-    // std::mt19937 gen(rd());
-    // double a = 0.1;
-    // std::uniform_real_distribution<double> dis(a, a);
-
-    //  for (int i = 0; i < m; ++i) {
-    //     for (int j = 0; j < n; ++j) {
-    //         payoff_matrix[i][j] = dis(gen);
-    //     }
-    // }
+    double alpha = 1e4;  // Example value of alpha
+    std::vector<int> omega_l = {0, 1, 2};  // Example mask omega_l: select the states of interest
 
     // example payoffs, TODO: compute payoff from data
     std::vector<std::vector<double>> payoff_matrix = {
@@ -37,10 +23,11 @@ int main() {
     std::vector<double> ask = {10.0 ,20.0};
     std::vector<double> bid = {9.0, 19.0};
 
-    Model::t M = new Model("example");
-    auto _M = finally([&]() { M->dispose(); }); // Automatically dispose the model at the end
+    // Initialization
+    Model::t M = new Model("main");
+    auto _M = finally([&]() { M->dispose(); });
 
-    // Define variables
+    // Define variables P and Q
     Variable::t p = M->variable("P", n, Domain::greaterThan(0.0));  // Variable P
     Variable::t q = M->variable("Q", n, Domain::greaterThan(0.0));  // Variable Q
 
@@ -52,59 +39,32 @@ int main() {
     for (int i = 0; i < m; ++i) {
         auto payoff_row = new_array_ptr<double>(payoff_matrix[i]);
         Expression::t dot_product = Expr::dot(payoff_row, q); // Dot product of payoff row and q
-
-        std::cout << i << std::endl;
-
-        // std::cout << "Dot product of payoff_matrix row " << i << " and q: " << dot_product->level() << std::endl;
-
         
         M->constraint("bid_" + std::to_string(i), dot_product, Domain::greaterThan(bid[i])); // Adding bid constraint
         M->constraint("ask_" + std::to_string(i), dot_product, Domain::lessThan(ask[i])); // Adding ask constraint
     }
 
-
-    typedef mosek::fusion::Variable M_Variable;
-    typedef mosek::fusion::Domain M_Domain;
-    typedef mosek::fusion::Expression M_Expression; 
-    typedef mosek::fusion::Expr M_Expr; 
-
-    M_Variable::t uu = M->variable(n, M_Domain::greaterThan(0.0)); // the moment matrix
-    M_Variable::t pp = M->variable(n, M_Domain::greaterThan(0.0)); // the diagonal
-    M_Variable::t qq = M->variable(n, M_Domain::greaterThan(0.0)); // the diagonal
-    M->constraint(M_Expr::hstack(M_Expr::mul(uu,0.5), qq, pp), M_Domain::inRotatedQCone());
-    M->constraint(M_Expr::sum(uu), M_Domain::lessThan(alpha)); 
-
-    // // Second moment constrain: introduce an auxiliary variable
-    // auto t = M->variable("t", Domain::greaterThan(0.0));
-
-    // // Second-order cone constraint: \sum p_i^2 <= alpha * \sum q_i
-    std::vector<Variable::t> p_square;
-    auto p_square_i = M->variable("P_SQ_" /*+ std::to_string(i)*/, n, Domain::greaterThan(0.0)); // Auxiliary variable for p[i]^2
-    for (int i = 0; i < n; ++i) {
-        M->constraint("cone_" + std::to_string(i), Expr::sub(p_square_i->index(i), Expr::eval(p->index(i)), Domain::equalsTo(0.0)); // p_square_i = p[i]
-        p_square.push_back(p_square_i);
+    // Create ancillary variables for the second moment of the pricing kernel constraint
+    Variable::t q_square = M->variable(n, Domain::greaterThan(0.0));
+    Variable::t p_square = M->variable(n, Domain::greaterThan(0.0));    
+    Variable::t one = M->variable(1, Domain::equalsTo(1.0));
+    
+    for(int i=0; i<n; ++i){
+        M->constraint("q_square" + std::to_string(i), Expr::hstack(Expr::mul(q_square->index(i), 0.5), one, q->index(i)), Domain::inRotatedQCone());
+        M->constraint("p_square" + std::to_string(i), Expr::hstack(Expr::mul(p_square->index(i), 0.5), one, p->index(i)), Domain::inRotatedQCone());
     }
 
-    //     std::vector<Variable::t> p_square;
-    // auto p_square_i = M->variable("P_SQ_" /*+ std::to_string(i)*/, n, Domain::greaterThan(0.0)); // Auxiliary variable for p[i]^2
-    // for (int i = 0; i < n; ++i) {
-    //     M->constraint("cone_" + std::to_string(i), Expr::sub(p_square_i->index(i), p->index(i)), Domain::equalsTo(0.0)); // p_square_i = p[i]
-    //     p_square.push_back(p_square_i);
-    // }
+    // minimize q_square so that q_square = q*q (same for p)
+    Expression::t obj_q_square = Expr::add(Expr::sum(q_square), Expr::sum(p_square));
+    M->objective("obj_q_square", ObjectiveSense::Minimize, obj_q_square);
 
-    // // Construct sum expressions for p^2 and alpha * q
-    // Expression::t sum_p_square = p_square[0];
-    // for (int i = 1; i < n; ++i) {
-    //     sum_p_square = Expr::add(sum_p_square, p_square[i]);
-    // }p_square
+    // Solve to get the equality
+    M->solve();
 
-    // Expression::t var_p_expr = Expr::constTerm(0.0);
-    // Expression::t var_q_expr = Expr::constTerm(0.0);
-    // for (int i = 0; i < n; ++i) {
-    //     var_p_expr = Expr::add(var_p_expr, Expr::mul(p_square[i], 1.0 / n)); // Var(p) = sum(p_i^2) / n
-    //     var_q_expr = Expr::add(var_q_expr, Expr::mul(q_square[i], 1.0 / n)); // Var(q) = sum(q_i^2) / n
-    // }
-    // M->constraint("variance_constraint", Expr::sub(var_p_expr, var_q_expr), Domain::greaterThan(0.0));
+    // Second moment of the pricing kernel constraint
+    for(int i=0; i<n; ++i){
+        M->constraint("second moment of the pricing kernel" + std::to_string(i), Expr::sub(Expr::mul(1/alpha, q_square->index(i)), p->index(i)), Domain::lessThan(0.0));
+    }
 
     // Define objective function using mask omega_l
     Expression::t obj_expr = Expr::constTerm(0.0);
@@ -117,8 +77,6 @@ int main() {
 
     // Solve the problem
     M->solve();
-    
-
 
     // Print solution
     auto sol_p = p->level();
@@ -126,9 +84,6 @@ int main() {
     std::cout << "Solution:" << std::endl;
     std::cout << "P = " << *sol_p << std::endl;
     std::cout << "Q = " << *sol_q << std::endl;
-
-    auto sol_ps = qq->level();
-    std::cout << "P^2 = " << *sol_ps << std::endl;
 
     // Print dot products after optimization
     std::cout << "Dot products after optimization:" << std::endl;
